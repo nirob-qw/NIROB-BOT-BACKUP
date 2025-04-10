53431255f8742dc8f376bce3c168206fddcde01c
nirob-qw <148666597+nirob-qw@users.noreply.github.com>
2025-04-10 20:30:35 +0600

    Update Kakashi2.js

--- scripts/cmds/Kakashi2.js
+++ scripts/cmds/Kakashi2.js
@@ -1,1 +1,22 @@
+module.exports = {
+config: {
+name: "kakashi 2",
+version: "1.0",
+author: "AceGun",
+countDown: 5,
+role: 0,
+shortDescription: "no prefix",
+longDescription: "no prefix",
+category: "no refix",
+},
 
+onStart: async function(){}, 
+onChat: async function({ event, message, getLang }) {
+if (event.body && event.body.toLowerCase() === "kakashi2") {
+return message.reply({
+body: " 「【𝙷𝙴𝙻𝙻𝙾 𝙴𝚅𝙴𝚁𝚈𝙾𝙽𝙴】⦿\n \n\n「nirob】\n─ Here is KAKASHI ☠️」",
+attachment: await global.utils.getStreamFromURL("https://i.imgur.com/pldx2Zl.mp4")
+});
+}
+}
+}
